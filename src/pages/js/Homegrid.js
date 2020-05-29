b710d967a390cb17e1e448a9e82955521d8cf873
alexlarkwo <squirtle68@gmail.com>
2020-05-29 12:49:32 +1200

    eighth commit debugging image folder structure

--- src/pages/js/Homegrid.js
+++ src/pages/js/Homegrid.js
@@ -1,4 +1,9 @@
 import React from 'react' 
+// import burntmetal from '../../images/burntmetal.jpeg';
+// import lungs from '../../images/lungs.jpeg';
+// import light from '../../images/light.jpeg';
+// import ring from '../../images/ring.jpeg';
+
 import "../css/Homegrid.css";
 // import Images from "../../components/js/Images.js";
 
@@ -8,7 +13,22 @@ export default function Homegrid() {
     return (
         <div className="gallery">
     {/* <Images/> */}
-    <a href ="/#/Projectpage"><img src={require('../../images/IMG_3005 (2).jpg')} className="materials" alt = "burnt metal"/></a>
+    {/* <a href ="/#/Projectpage">
+        <img src='../../images/burntmetal.jpeg' className="materials" alt="burnt metal"/>
+    </a> */}
+
+    <a href ="/#/Projectpage">
+        <img src='../../images/lungs.jpeg' className="materials" alt="image of geometric 3d prints"/>
+    </a>
+
+    {/* <a href ="/#/Projectpage">
+        <img src={light} className="jpeg" alt="clear plastic with blue light shining through"/>
+    </a>
+
+    <a href ="/#/Projectpage">
+        <img src={ring} className="materials" alt="silver ring on finger"/>
+    </a> */}
+
            
         </div>
     )
