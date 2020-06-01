47db4deb51e63fa914893d4a042c5f08efc41dd3
blaineanthonywestern <blaine.a.western@gmail.com>
2020-06-01 17:21:19 +1200

    add changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { HashRouter, Route } from "react-router-dom";
-=======
 import { BrowserRouter as Router, Route } from "react-router-dom";
->>>>>>> e43611b76abb77742e5fed368124924bbaa8b1e3
 
 import "./App.css";
 
@@ -15,30 +11,15 @@ import EmailForm from "./pages/js/EmailForm";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <HashRouter basename="/Homegrid">
-      <div className="App">
-        <Nav />
-
-        {/* <EmailForm /> */}
-
-        <Route path="/homegrid" exact component={Homegrid} />
-        <Route path="/projectpage" exact component={Projectpage} />
-        <Route path="/emailform" exact component={EmailForm} />
-        <Footer />
-      </div>
-    </HashRouter>
-=======
-    <Router>
-      <div className="App">
+    <div className="App">
+      <Router>
         <Nav />
         <Route path="/" exact component={Homegrid} />
         <Route path="/projectpage" exact component={Projectpage} />
         {/* <Route path = "/emailform" exact component={EmailForm}/>  */}
         <Footer />
-      </div>
-    </Router>
->>>>>>> e43611b76abb77742e5fed368124924bbaa8b1e3
+      </Router>
+    </div>
   );
 }
 
